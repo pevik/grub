2bbefc61c9aa3ad495835320a4d56afd5ea96619
Petr Vorel <pvorel@suse.cz>
2021-08-02 15:44:30 +0200

    backport 1ea4e5ef0 ("osdep/linux/hostdisk: Use stat() instead of udevadm for partition lookup")
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- grub-core/osdep/linux/hostdisk.c
+++ grub-core/osdep/linux/hostdisk.c
@@ -104,61 +104,13 @@ grub_util_get_fd_size_os (grub_util_fd_t fd, const char *name, unsigned *log_sec
 static char *
 sysfs_partition_path (const char *dev, const char *entry)
 {
-  const char *argv[7];
-  int fd;
-  pid_t pid;
-  FILE *udevadm;
-  char *buf = NULL;
-  size_t len = 0;
-  char *path = NULL;
   struct stat st;
-  int ret;
 
-  ret = stat(dev, &st);
-  if (ret == 0 && S_ISBLK(st.st_mode))
+  if (stat (dev, &st) == 0 && S_ISBLK (st.st_mode))
     return xasprintf ("/sys/dev/block/%u:%u/%s",
 		      major (st.st_rdev), minor (st.st_rdev), entry);
 
-  argv[0] = "udevadm";
-  argv[1] = "info";
-  argv[2] = "--query";
-  argv[3] = "path";
-  argv[4] = "--name";
-  argv[5] = dev;
-  argv[6] = NULL;
-
-  pid = grub_util_exec_pipe (argv, &fd);
-
-  if (!pid)
-    return NULL;
-
-  /* Parent.  Read udevadm's output.  */
-  udevadm = fdopen (fd, "r");
-  if (!udevadm)
-    {
-      grub_util_warn (_("Unable to open stream from %s: %s"),
-		      "udevadm", strerror (errno));
-      close (fd);
-      goto out;
-    }
-
-  if (getline (&buf, &len, udevadm) > 0)
-    {
-      char *newline;
-
-      newline = strchr (buf, '\n');
-      if (newline)
-	*newline = '\0';
-      path = xasprintf ("/sys%s/%s", buf, entry);
-    }
-
-out:
-  if (udevadm)
-    fclose (udevadm);
-  waitpid (pid, NULL, 0);
-  free (buf);
-
-  return path;
+  return NULL;
 }
 
 static int
